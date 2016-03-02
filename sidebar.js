b5411227d7c6b32828a491686b748f148f3094b4
twiveyp <patrick.twivey@gmail.com>
2016-03-02 14:29:40 +1000

    Update

---
+++ sidebar.js
@@ -0,0 +1,12 @@
+function slide() {
+	var value = document.getElementsByTagName("button")[0].getAttribute("value");
+	var sidebar = document.getElementById("sidebarhidden");
+
+	if (value === "off") {
+		document.getElementsByTagName("button")[0].setAttribute("value", "on");
+		sidebar.style.marginLeft = "0";
+	} else {
+		document.getElementsByTagName("button")[0].setAttribute("value", "off");
+		sidebar.style.marginLeft = "-20vw";
+	}
+}
